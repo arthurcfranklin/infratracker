c06c3f4d91b522bac329fecf057084e4f1665435
arthurcfranklin <arthurdcaf@gmail.com>
2026-06-07 18:15:15 -0300

    Add dashboard asset deletion feature

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
-
+from fastapi import Request
+from src.app.database.db import SessionLocal
+from src.app.models.asset import Asset
+from fastapi.templating import Jinja2Templates
 from src.app.api.assets import router as assets_router
 from src.app.database.db import Base, engine
-from src.app.models.asset import Asset
+from fastapi.responses import RedirectResponse
+
+
+templates = Jinja2Templates(directory="src/app/templates")
 
 Base.metadata.create_all(bind=engine)
 
@@ -22,3 +28,53 @@ def root():
         "status": "running",
         "version": "0.1.0-alpha",
     }
+
+@app.get("/dashboard")
+def dashboard(request: Request):
+
+    db = SessionLocal()
+
+    assets = db.query(Asset).all()
+
+    total_assets = len(assets)
+
+    online_assets = len(
+        [asset for asset in assets if asset.status.lower() == "online"]
+    )
+
+    offline_assets = len(
+        [asset for asset in assets if asset.status.lower() == "offline"]
+    )
+
+    db.close()
+
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={
+            "assets": assets,
+            "total_assets": total_assets,
+            "online_assets": online_assets,
+            "offline_assets": offline_assets,
+        },
+    )
+
+@app.get("/assets/delete/{asset_id}")
+def delete_asset_web(asset_id: int):
+
+    db = SessionLocal()
+
+    asset = db.query(Asset).filter(
+        Asset.id == asset_id
+    ).first()
+
+    if asset:
+        db.delete(asset)
+        db.commit()
+
+    db.close()
+
+    return RedirectResponse(
+        url="/dashboard",
+        status_code=303
+    )
